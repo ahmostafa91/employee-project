e2a2c8e7d8eb85e7d90047b6fc09db6070ce12e5
ahmed <ahmedmostafa05031991@gmail.com>
2019-03-12 16:49:51 +0200

    moving the filter from pipe to C

--- src/app/employee/list-employees.component.ts
+++ src/app/employee/list-employees.component.ts
@@ -11,17 +11,43 @@ import { Router } from '@angular/router';
 export class ListEmployeesComponent implements OnInit {
 
   employees: Employee[];
-  searchTearm: string; // take the name from search input
+
+  private _searchTerm: string; // take the name from search input
+  // We are binding to this property in the view template, so this
+  // getter is called when the binding needs to read the value
+
   // dataFromChild: string; // to get data from child
+
+  // Use this property to stored filtered employees, so we do not
+  // lose the original list and do not have to make a round trip
+  // to the web server on every new search
+  filteredEmployees: Employee[];
+
+
+  get searchTerm(): string {
+    return this._searchTerm;
+  }
+
+  // This setter is called everytime the value in the search text box changes
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredEmployees = this.filterEmployees(value);
+  }
 
   constructor(private _employeeService: EmployeeService, private _router: Router) { }
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployee();
+    this.filteredEmployees = this.employees;
   }
 
   onClick(empId: number) {
     this._router.navigate(['/employee', empId]);
+  }
+
+  filterEmployees(searchString: string) {
+    return this.employees.filter(employee =>
+      employee.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
   }
 
   /*fromChild(dataEvent: string) {
